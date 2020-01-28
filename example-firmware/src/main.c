2eaac9f4a965939c9495e31e9ebcfedff31cd9fd
Wojciech Tatarski <wtatarski@antmicro.com>
2020-01-28 16:07:00 +0100

    Control EQ pins of video booster

--- example-firmware/src/main.c
+++ example-firmware/src/main.c
@@ -15,18 +15,39 @@
 static struct device *i2c_dev;
 static struct device *led0;
 static struct device *led1;
+static struct device *tx_eq0;
+static struct device *tx_eq1;
+
+#define TX_EQ0_PORT DT_GPIO_GECKO_PORTA_NAME
+#define TX_EQ0_PIN 9
+#define TX_EQ1_PORT DT_GPIO_GECKO_PORTA_NAME
+#define TX_EQ1_PIN 10
 
 static int i2c_cb(struct i2c_slave_config *config, uint8_t val)
 {
-        static uint8_t cnt;
+	if (val == 0x0) {
+		gpio_pin_write(tx_eq0, TX_EQ0_PIN, 0);
+		gpio_pin_write(tx_eq1, TX_EQ1_PIN, 0);
+		gpio_pin_write(led0, DT_ALIAS_LED0_GPIOS_PIN, 0);
+		gpio_pin_write(led1, DT_ALIAS_LED1_GPIOS_PIN, 0);
+	} else if (val == 0x1) {
+		gpio_pin_write(tx_eq0, TX_EQ0_PIN, 1);
+		gpio_pin_write(tx_eq1, TX_EQ1_PIN, 0);
+		gpio_pin_write(led0, DT_ALIAS_LED0_GPIOS_PIN, 1);
+		gpio_pin_write(led1, DT_ALIAS_LED1_GPIOS_PIN, 0);
+	} else if (val == 0x2) {
+		gpio_pin_write(tx_eq0, TX_EQ0_PIN, 0);
+		gpio_pin_write(tx_eq1, TX_EQ1_PIN, 1);
+		gpio_pin_write(led0, DT_ALIAS_LED0_GPIOS_PIN, 0);
+		gpio_pin_write(led1, DT_ALIAS_LED1_GPIOS_PIN, 1);
+	} else if (val == 0x3) {
+		gpio_pin_write(tx_eq0, TX_EQ0_PIN, 1);
+		gpio_pin_write(tx_eq1, TX_EQ1_PIN, 1);
+		gpio_pin_write(led0, DT_ALIAS_LED0_GPIOS_PIN, 1);
+		gpio_pin_write(led1, DT_ALIAS_LED1_GPIOS_PIN, 1);
+	}
 
-        if (val == 0xAA) {
-                gpio_pin_write(led0, DT_ALIAS_LED0_GPIOS_PIN, cnt % 2);
-                gpio_pin_write(led1, DT_ALIAS_LED1_GPIOS_PIN, cnt % 2);
-                cnt++;
-        }
-
-        return 0;
+	return 0;
 }
 
 static const struct i2c_slave_callbacks callbacks = {
@@ -40,7 +61,6 @@ void main(void)
 	if (!i2c_dev)
 		printf("binding failed\n");
 
-
         /* Disable watchdog timer */
         *(uint32_t *)0x40088000UL = 0;
 
@@ -50,10 +70,21 @@ void main(void)
         if (!led0 && !led1)
 		printf("binding leds failed\n");
 
+	tx_eq0 = device_get_binding(TX_EQ0_PORT);
+	tx_eq1 = device_get_binding(TX_EQ1_PORT);
+
+	if (!tx_eq0 && !tx_eq1)
+		printf("binding equalizer setting pins failed\n");
+
         gpio_pin_configure(led0, DT_ALIAS_LED0_GPIOS_PIN, GPIO_DIR_OUT);
         gpio_pin_write(led0, DT_ALIAS_LED0_GPIOS_PIN, 0);
         gpio_pin_configure(led1, DT_ALIAS_LED1_GPIOS_PIN, GPIO_DIR_OUT);
         gpio_pin_write(led1, DT_ALIAS_LED1_GPIOS_PIN, 0);
+
+	gpio_pin_configure(tx_eq0, TX_EQ0_PIN, GPIO_DIR_OUT);
+	gpio_pin_write(tx_eq0, TX_EQ0_PIN, 0);
+	gpio_pin_configure(tx_eq1, TX_EQ1_PIN, GPIO_DIR_OUT);
+	gpio_pin_write(tx_eq1, TX_EQ1_PIN, 0);
 
         struct i2c_slave_config cfg;
         cfg.address = 0x74;
